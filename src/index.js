5dbd8e5ecdb4bf3b957e6131acc9885b68497d4d
trinhquan1004 <trinhquan04102002@gmail.com>
2023-05-31 15:46:40 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const errorHandler = require('./middlewares/errorHandler');
+
+require('dotenv').config();
+require('./models');
+
+const { PORT } = require('./configs');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`App listening on ${PORT}`);
+});
