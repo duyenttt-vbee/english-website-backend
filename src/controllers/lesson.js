92beaf7f7e1b4713a7b3db3b9bbce5b24cea3d9b
trinhquan1004 <trinhquan04102002@gmail.com>
2023-06-07 15:49:05 +0700

    Create apis get lessons and get lesson

--- src/controllers/lesson.js
+++ src/controllers/lesson.js
@@ -6,4 +6,15 @@ const createLesson = async (req, res) => {
   return res.send({ status: 1, result: { lesson } });
 };
 
-module.exports = { createLesson };
+const getAllLessons = async (req, res) => {
+  const lessons = await lessonDao.getAllLessons();
+  return res.send({ status: 1, result: { lessons } });
+};
+
+const getLesson = async (req, res) => {
+  const { id } = req.params;
+  const lesson = await lessonDao.getLesson(id);
+  return res.send({ status: 1, result: { lesson } });
+};
+
+module.exports = { createLesson, getAllLessons, getLesson };
